bc85300f90c693b29385343976baae91101aa384
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-08-30 15:36:06 +0430

    fix bug theme

--- src/components/Header/Theme.tsx
+++ src/components/Header/Theme.tsx
@@ -5,24 +5,18 @@ import { useTheme } from "next-themes";
 
 const Theme = (): JSX.Element => {
   const { theme, setTheme } = useTheme();
-  
-  const [imgTheme, setImgTheme] = useState(
-    theme === "dark" ? SunIcon : MoonIcon
-  );
 
   const changeTheme = () => {
     if (theme === "dark") {
       setTheme("light");
-      setImgTheme(MoonIcon);
     } else {
       setTheme("dark");
-      setImgTheme(SunIcon);
     }
   };
 
   return (
     <button title="Theme" onClick={changeTheme} className="btn">
-      <img src={imgTheme} width="25" alt="" />
+      <img src={theme === "darke" ? SunIcon : MoonIcon} width="25" alt="" />
     </button>
   );
 };
