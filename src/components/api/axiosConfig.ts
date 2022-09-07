784167a05a8e343c751efa7efe414c674256a30b
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-09-07 17:11:41 +0430

    change ApiKey

--- src/components/api/axiosConfig.ts
+++ src/components/api/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const Axios = axios.create({
   baseURL: "https://api-football-beta.p.rapidapi.com",
   headers: {
-    "X-RapidAPI-Key": "7f6f6cea8cmsh5f0c1be9deaaf9bp145633jsn354fcc9716d0",
+    "X-RapidAPI-Key": "0437699d1emshb81897d3deb22ccp15680ajsn4266970eb200",
     "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
   },
 });
